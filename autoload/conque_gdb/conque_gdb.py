9537c0b482c12762b8be3e3cec630d1d0f6f63b7
yinchen <yinchen@freewheel.tv>
2019-04-28 08:26:54 +0800

    Depracation warning with Python 3

--- autoload/conque_gdb/conque_gdb.py
+++ autoload/conque_gdb/conque_gdb.py
@@ -1,4 +1,4 @@
-import re, collections
+import re, collections, collections.abc
 
 # Marks that a breakpoint has been hit
 GDB_BREAK_MARK = '\x1a\x1a'
@@ -36,7 +36,7 @@ class RegisteredBreakpoint:
     def __str__(self):
         return self.filename + ':' + self.lineno + ',' + self.enabled
 
-class RegisteredBpDict(collections.MutableMapping):
+class RegisteredBpDict(collections.abc.MutableMapping):
     def __init__(self):
         self.r_breaks = dict()
         self.lookups = dict()
@@ -102,7 +102,7 @@ class ConqueGdb(Conque):
     # Breakpoints which have been registered to exist
     registered_breakpoints = RegisteredBpDict()
 
-    # Mapping from linenumber + filename to a tuple containing the id of the sign 
+    # Mapping from linenumber + filename to a tuple containing the id of the sign
     # placed there and whether the breakpoint is enabled ('y') or disabled ('n')
     lookup_sign_ids = dict()
 
